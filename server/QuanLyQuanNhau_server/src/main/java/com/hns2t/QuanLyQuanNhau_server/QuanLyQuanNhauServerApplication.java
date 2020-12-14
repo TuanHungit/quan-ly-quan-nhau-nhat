aceff10a7203879739a5c7d6252278829b6d432d
NganDo2911 <dothithanhngan2911@gmail.com>
2020-12-14 12:02:13 +0700

    init folder backend

---
+++ server/QuanLyQuanNhau_server/src/main/java/com/hns2t/QuanLyQuanNhau_server/QuanLyQuanNhauServerApplication.java
@@ -0,0 +1,13 @@
+package com.hns2t.QuanLyQuanNhau_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuanLyQuanNhauServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuanLyQuanNhauServerApplication.class, args);
+	}
+
+}
