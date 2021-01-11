ef85fea2dec5e7ddee49b001fda80b25ec2348e6
NganDo2911 <dothithanhngan2911@gmail.com>
2021-01-11 16:26:23 +0700

    api delete hoadon completed

--- server/QuanLyQuanNhau_server/src/main/java/com/hns2t/QuanLyQuanNhau_server/dao/HoaDonRepository.java
+++ server/QuanLyQuanNhau_server/src/main/java/com/hns2t/QuanLyQuanNhau_server/dao/HoaDonRepository.java
@@ -5,10 +5,6 @@ import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
-import org.springframework.format.annotation.DateTimeFormat;
-import org.springframework.web.bind.annotation.PathVariable;
-
-import com.hns2t.QuanLyQuanNhau_server.model.ChiTietHoaDon;
 import com.hns2t.QuanLyQuanNhau_server.model.HoaDon;
 
 public interface HoaDonRepository extends JpaRepository<HoaDon, Long> {
